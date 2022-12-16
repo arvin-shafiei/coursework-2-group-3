91fbdfb81a28b57f7cbd77832056b474d6d06be4
Joe Rogan <114414426+arvin-shafiei@users.noreply.github.com>
2022-12-16 21:26:42 +0000

    Possible Fixes

--- Trending/script.js
+++ Trending/script.js
@@ -1,6 +1,7 @@
 const trending = "https://api.coingecko.com/api/v3/search/trending";
 var trendinglist = [];
 
+//FETCH TRENDING LIST
 fetch(trending)
   .then((resp) => resp.json())
   .then(function (coinData) {
@@ -14,6 +15,7 @@ fetch(trending)
     console.log(error);
   });
 
+// FETCH COINBASE PRICE
 function getData(url) {
   fetch(url)
     .then((resp) => resp.json())
@@ -26,41 +28,57 @@ function getData(url) {
       console.log(error);
     });
 }
+function DisplayCards() {
 
-function displayCards() {
   for (let result of trendinglist) {
     let text =
       `<div class="col lg-4">
-          <div class="card mb-3">
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item white-bg">
-                  <div>
-                    <p>
-                       <img class="img-btc" src=` + result.image.large + ` alt="coin"/>
-                    <   
-                    <p>
-                        <span class="black">` + result.name + ` (` + result.symbol + `) </span>
-                    <   
-                    <span class="fs-6 text text-muted">$ ` + result.market_data.current_price.usd + `</span>
-                  <div>
-                </li>
-            </    
-            <div class="card-body">
-                <p class="desc">` + result.description.en + `</p>
-            </d   
-            <div>
-              <canvas id=` + result.id + `></canvas>
-            </div>
-               
-            <div class="buy-btn btn btn-success"> 
-                <a href=` + result.links.homepage[0] + ` target="_blank" >Buy</a>
-            </div>
-          </div>
-        </div>`;
+            <div class="card mb-3">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item white-bg">
+
+                    <div>
+                        <p>
+                           <img class="img-btc" src=` +
+      result.image.large +
+      ` alt="coin"/>
+                        </p>
+                        <p>
+                            <span class="black">` +
+      result.name + ` (` + result.symbol +
+      `)</span>
+                        </p>
+                        <span class="fs-6 text text-muted">$ ` +
+      result.market_data.current_price.usd +
+      `</span>
+       <div>
+                    </li>
+
+                </ul>
+                <div class="card-body">
+                    <p class="desc">
+                    ` +
+      result.description.en +
+      `
+      </p>
+   
+            
+                    </div>
+                    <div>
+                    <canvas id=`+ result.id + `></canvas>
+                    </div>
+                   
+                    <div class="buy-btn btn btn-success"> 
+                    <a href=` +
+      result.links.homepage[0] +
+      ` target="_blank" >Buy</a>
+      </div>
+                </div>
+            </div>`;
+
     $("#trendingCrypto").append(text);
   }
 }
-
 function initializeCharts(id, dataset) {
   const ctx = document.getElementById(id);
   console.log(dataset)
@@ -74,6 +92,8 @@ function initializeCharts(id, dataset) {
         label: '',
         borderColor: '#36A2EB',
         backgroundColor: '#9BD0F5',
+
+        // borderWidth: 1
       }]
     },
     options: {
@@ -95,11 +115,14 @@ function initializeCharts(id, dataset) {
   });
 }
 setTimeout(() => {
-  displayCards();
+  DisplayCards();
+
 }, 3000);
 
 setTimeout(() => {
   trendinglist.map((data) => {
+    console.log(data.market_data.sparkline_7d);
     initializeCharts(data.id, data.market_data.sparkline_7d);
+
   })
 }, 4000);
--- script.js
+++ script.js
@@ -1,115 +1,118 @@
 
-function timeConverter(UNIX_timestamp) {
-  var a = new Date(UNIX_timestamp);
-  var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  var year = a.getYear() + 1900;
-  var month = months[a.getMonth()];
-  var date = a.getDate();
-  var time = date + ' ' + month + ' ' + year + ' ';
-  return time;
-}
-
-const coinGeckobtc = 'https://api.coingecko.com/api/v3/coins/bitcoin/ohlc?vs_currency=usd&days=max';
-const coinGeckoeth = 'https://api.coingecko.com/api/v3/coins/ethereum/ohlc?vs_currency=usd&days=max';
-const coinGeckoltc = 'https://api.coingecko.com/api/v3/coins/litecoin/ohlc?vs_currency=usd&days=max';
-
-let chartData1, chartData2, chartData3 = []
-
-fetch(coinGeckobtc).then((resp) => resp.json()).then(function (coinData1) {
-  console.log(coinData1 + " CoinGekobtc");
-  chartData1 = coinData1
-}).
-  catch(function (error) {
-    console.log(error);
-  });
-
-var currentWait = 0.0;
-
-var interval = setInterval(function () {
-  if ((chartData1[0]) || currentWait >= 5) {
-
-    for (let i = 0; i < chartData1.length; i++) {
-      chartData1[i][0] = timeConverter(chartData1[i][0])
-    }
-    var data = google.visualization.arrayToDataTable(chartData1, true);
-
-    var options = {
-      legend: 'none'
-    }
-
-    var chart = new google.visualization.CandlestickChart(document.getElementById('chart_btc'));
-
-    chart.draw(data, options);
-    clearInterval(interval);
-  } else {
-    console.log('Retriving BTC Prices');
-    currentWait += 0.5;
-  }
-},
-  500);
-
-fetch(coinGeckoeth).then((resp) => resp.json()).then(function (coinData2) {
-  console.log(coinData2 + " CoinGekoeth");
-  chartData2 = coinData2
-}).
-  catch(function (error) {
-    console.log(error);
-  });
-
-var currentWait2 = 0.0;
-var interval2 = setInterval(function () {
-
-  if ((chartData2[0]) || currentWait2 >= 5) {
-    for (let i = 0; i < chartData2.length; i++) {
-      chartData2[i][0] = timeConverter(chartData2[i][0])
-    }
-    var data = google.visualization.arrayToDataTable(chartData2, true);
-
-    var options = {
-      legend: 'none'
-    }
-
-    var chart = new google.visualization.CandlestickChart(document.getElementById('chart_eth'));
-
-    chart.draw(data, options);
-    clearInterval(interval2);
-  } else {
-    console.log('Retriving ETH Prices');
-    currentWait2 += 0.5;
+document.addEventListener("DOMContentLoaded", () => {
+  function timeConverter(UNIX_timestamp) {
+    var a = new Date(UNIX_timestamp);
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var year = a.getYear() + 1900;
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var time = date + ' ' + month + ' ' + year + ' ';
+    return time;
   }
 
-},
-  500);
+  const coinGeckobtc = 'https://api.coingecko.com/api/v3/coins/bitcoin/ohlc?vs_currency=usd&days=max';
+  const coinGeckoeth = 'https://api.coingecko.com/api/v3/coins/ethereum/ohlc?vs_currency=usd&days=max';
+  const coinGeckoltc = 'https://api.coingecko.com/api/v3/coins/litecoin/ohlc?vs_currency=usd&days=max';
 
-fetch(coinGeckoltc).then((resp) => resp.json()).then(function (coinData3) {
-  console.log(coinData3 + " CoinGekoltc");
-  chartData3 = coinData3
-}).
-  catch(function (error) {
-    console.log(error);
-  });
+  let chartData1 = [];
+  let chartData2 = [];
+  let chartData3 = [];
 
-var currentWait3 = 0.0;
-var interval3 = setInterval(function () {
-  if ((chartData3[0]) || currentWait3 >= 5) {
+  fetch(coinGeckobtc).then((resp) => resp.json()).then(function (coinData1) {
+    console.log(coinData1 + " CoinGekobtc");
+    chartData1 = coinData1
+  }).
+    catch(function (error) {
+      console.log(error);
+    });
 
-    for (let i = 0; i < chartData3.length; i++) {
-      chartData3[i][0] = timeConverter(chartData3[i][0])
+  var currentWait = 0.0;
+
+  var interval = setInterval(function () {
+    if ((chartData1[0]) || currentWait >= 5) {
+
+      for (let i = 0; i < chartData1.length; i++) {
+        chartData1[i][0] = timeConverter(chartData1[i][0])
+      }
+      var data = google.visualization.arrayToDataTable(chartData1, true);
+
+      var options = {
+        legend: 'none'
+      }
+
+      var chart = new google.visualization.CandlestickChart(document.getElementById('chart_btc'));
+
+      chart.draw(data, options);
+      clearInterval(interval);
+    } else {
+      console.log('Retriving BTC Prices');
+      currentWait += 0.5;
     }
-    var data = google.visualization.arrayToDataTable(chartData3, true);
+  },
+    500);
 
-    var options = {
-      legend: 'none'
+  fetch(coinGeckoeth).then((resp) => resp.json()).then(function (coinData2) {
+    console.log(coinData2 + " CoinGekoeth");
+    chartData2 = coinData2
+  }).
+    catch(function (error) {
+      console.log(error);
+    });
+
+  var currentWait2 = 0.0;
+  var interval2 = setInterval(function () {
+
+    if ((chartData2[0]) || currentWait2 >= 5) {
+      for (let i = 0; i < chartData2.length; i++) {
+        chartData2[i][0] = timeConverter(chartData2[i][0])
+      }
+      var data = google.visualization.arrayToDataTable(chartData2, true);
+
+      var options = {
+        legend: 'none'
+      }
+
+      var chart = new google.visualization.CandlestickChart(document.getElementById('chart_eth'));
+
+      chart.draw(data, options);
+      clearInterval(interval2);
+    } else {
+      console.log('Retriving ETH Prices');
+      currentWait2 += 0.5;
     }
 
-    var chart = new google.visualization.CandlestickChart(document.getElementById('chart_ltc'));
+  },
+    500);
 
-    chart.draw(data, options);
-    clearInterval(interval3);
+  fetch(coinGeckoltc).then((resp) => resp.json()).then(function (coinData3) {
+    console.log(coinData3 + " CoinGekoltc");
+    chartData3 = coinData3
+  }).
+    catch(function (error) {
+      console.log(error);
+    });
 
-  } else {
-    console.log('Retriving LTC Prices');
-    currentWait3 += 0.5;
-  }
-}, 500);
+  var currentWait3 = 0.0;
+  var interval3 = setInterval(function () {
+    if ((chartData3[0]) || currentWait3 >= 5) {
 
+      for (let i = 0; i < chartData3.length; i++) {
+        chartData3[i][0] = timeConverter(chartData3[i][0])
+      }
+      var data = google.visualization.arrayToDataTable(chartData3, true);
+
+      var options = {
+        legend: 'none'
+      }
+
+      var chart = new google.visualization.CandlestickChart(document.getElementById('chart_ltc'));
+
+      chart.draw(data, options);
+      clearInterval(interval3);
+
+    } else {
+      console.log('Retriving LTC Prices');
+      currentWait3 += 0.5;
+    }
+  }, 500);
+});
